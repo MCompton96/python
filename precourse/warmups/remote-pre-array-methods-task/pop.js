281399336f8455006a1066a3f04c957a30c749f2
MCompton96 <matthewcompton49@gmail.com>
2020-08-13 08:54:28 +0100

    completed

---
+++ precourse/warmups/remote-pre-array-methods-task/pop.js
@@ -0,0 +1,10 @@
+const breakfastFoods = ['toast', 'cereal', 'full english', 'croissant'];
+const mystery1 = breakfastFoods.pop();
+
+// What do you predict this will log?
+// breakfastFoods = ['toast', 'cereal', 'full english',];
+console.log(breakfastFoods);
+
+// What value do you think this will log?
+// mystery1 = 'croissant';
+console.log(mystery1);
---
+++ precourse/warmups/remote-pre-array-methods-task/push.js
@@ -0,0 +1,11 @@
+const drinks = ['coffee', 'tea', 'orange juice'];
+const mystery2 = drinks.push('milk');
+
+// What do you predict this will log?
+// drinks = ['coffee', 'tea', 'orange juice', 'milk'];
+console.log(drinks);
+
+// What value do you think this will log?
+// mystery2 = 4;
+// If you were surprised at the value, research online what this value represents
+console.log(mystery2);
